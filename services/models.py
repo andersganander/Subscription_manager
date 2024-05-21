c92fbf4eef7383b98e51a8816f2d98cc0d43e2c0
Anders Ganander <anders.ganander@gmail.com>
2024-05-21 13:25:32 +0000

    Create model #19

--- services/models.py
+++ services/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+  
+  
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    
+    # Add validation to check if it's an FA icon
+    icon = models.CharField(max_length=200, help_text='Add the name of the Font Awesome icon')
+    
+    # Change to color picker
+    # https://mackenzie.morgan.name/posts/django-admin-color-picker/
+    color = models.CharField(max_length=50)
