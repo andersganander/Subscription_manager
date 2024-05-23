35000db923cff49e535c64c47dcd5f72012a32d3
Anders Ganander <anders.ganander@gmail.com>
2024-05-23 13:59:28 +0000

    Add templates for login and signup #27

--- subscription_manager/urls.py
+++ subscription_manager/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('services/', include("services.urls")),
+    path('accounts/', include('allauth.urls')),
 ]
