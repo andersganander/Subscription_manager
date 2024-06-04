4d6bd6b1abfac7fb6e3f6b98554dfbdd09233305
Anders Ganander <anders.ganander@gmail.com>
2024-06-04 08:10:54 +0000

    Add fields in subscription detail view #44

--- subscriptions/forms.py
+++ subscriptions/forms.py
@@ -8,8 +8,22 @@ class SubscriptionForm(forms.ModelForm):
         fields = 'service', 'monthly_price','expiry_date'
     
 
-class SubscriptionEditForm(forms.ModelForm):
+# class SubscriptionEditForm(forms.ModelForm):
     
+#     class Meta:
+#         model = Subscription
+#         fields = '__all__'
+
+
+class SubscriptionEditForm(forms.ModelForm):
+   
     class Meta:
         model = Subscription
-        fields = '__all__'
+        fields = ['monthly_price', 'notes', 'expiry_date', 'active', 'service', 'subscriber', 'subscription_reminder', 'paymentmethod', 'subscription_category']
+        widgets = {
+            'monthly_price': forms.NumberInput(attrs={'class': 'validate'}),
+            'notes': forms.Textarea(attrs={'class': 'materialize-textarea'}),
+            'expiry_date': forms.DateInput(format=('%Y-%m-%d'), attrs={'class': 'datepicker'}),
+            'active': forms.CheckboxInput(),
+            'paymentmethod': forms.TextInput(attrs={'class': 'validate'}),
+        }
