34e55f2158c0b36ec4a125073d3aa0763fe8de7c
Anders Ganander <anders.ganander@gmail.com>
2024-06-04 20:19:03 +0000

    Add style to subcription edit form #42

--- subscriptions/forms.py
+++ subscriptions/forms.py
@@ -18,10 +18,16 @@ class SubscriptionForm(forms.ModelForm):
 class SubscriptionEditForm(forms.ModelForm):
    
     class Meta:
+
         model = Subscription
-        fields = ['monthly_price', 'notes', 'active', 'service', 'subscriber', 'subscription_category']
+        fields = ['active', 'monthly_price', 'subscription_category', 'notes', 'reminder_date', 'reminder_notes' ]
         widgets = {
+            'active': forms.Select(attrs={'class': 'browser-default'}, choices=[[True,'Yes'],[False,'No']]),
             'monthly_price': forms.NumberInput(attrs={'class': 'validate'}),
             'notes': forms.TextInput(attrs={'class': 'validate'}),
-            'active': forms.CheckboxInput(),
+            'reminder_date': forms.DateInput(attrs={'class': 'datepicker'}),
+            'reminder_notes': forms.TextInput(attrs={'class': 'validate'}),
+            'subscription_category': forms.Select(attrs={'class': 'browser-default'}), 
         }
+
+        # 'active': forms.NullBooleanSelect(attrs={'class': 'browser-default'}),
