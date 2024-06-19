69267b516cac8a1ebdb2b50f891a4c41b440decc
Anders Ganander <anders.ganander@gmail.com>
2024-06-19 10:23:50 +0000

    Refactor code #77

--- subscriptions/management/commands/send_reminder_email.py
+++ subscriptions/management/commands/send_reminder_email.py
@@ -10,30 +10,33 @@ class Command(BaseCommand):
     help = 'Sends reminders to subscribers'
 
     def handle(self, *args, **options):
-        try:
-            print(f"HOST_USER: {settings.EMAIL_HOST_USER}")
-            print(f"HOST: {settings.EMAIL_HOST}")
+        """
+        This function iterates over all subscriptions, checks if a reminder needs to be sent, 
+        and sends the reminder email if necessary.
 
+        Parameters:
+        *args: Not used in this function.
+        **options: Not used in this function.
+
+        Returns:
+        None
+        """
+        try:
             subscriptions = Subscription.objects.all()
             today = datetime.date.today()
             for sub in subscriptions:
-                #self.stdout.write(self.style.SUCCESS(f"{today} {sub.subscription_name} {sub.reminder_date}"))
-                print(f"sub.subscription_name = {sub.subscription_name}")
+                self.stdout.write(self.style.SUCCESS(f"sub.subscription_name: {sub.subscription_name}"))
                 if sub.reminder_date == today and sub.active == True:
                     self.stdout.write(self.style.SUCCESS(f"Reminder: {sub.subscription_name}"))
-                    # send mail 
+                    # compose mail 
                     subject = 'Reminder from Subscription Manager'
-                    # user_email = User.objects.get(username=sub.subscriber).email
                     user_email = sub.reminder_email
                     message = f"Hi {sub.subscriber}, here is your reminder for {sub.subscription_name}.\r\n\r\n{sub.reminder_notes}\r\n\r\nThe Subscription Manager Team"
-                    #message = f'{sub.reminder_notes}'
                     email_from = settings.EMAIL_HOST_USER
-                    #print(settings.EMAIL_HOST_USER)
-                    #user = User.objects.get(id=2)
-                    #user_email = User.objects.get(username=2).user.email
                     if user_email and len(user_email) > 0:
                         print(user_email)
                         recipient_list = [user_email]
+                        # send mail
                         send_mail( subject, message, email_from, recipient_list )
                         self.stdout.write(self.style.SUCCESS(f"Mail sent to {user_email} for {sub.subscription_name}"))
                     else:
@@ -42,7 +45,7 @@ class Command(BaseCommand):
                     self.stdout.write(self.style.SUCCESS(f"No reminder for {sub.subscription_name}"))
 
         except FieldDoesNotExist:
-            self.stdout.write(self.style.ERROR('Field "title" does not exist.'))
+            self.stdout.write(self.style.ERROR('Field does not exist.'))
             return
 
         self.stdout.write(self.style.SUCCESS('Successfully iterated all subscriptions.'))
